8fa7ab686e354dd3281f5796b6a8b7b283abd13e
Shwetank Singh <ssingh.21.2019@gmail.com>
2023-07-18 22:57:16 +0530

    upload01
    - destroying of enemy is made and a destroy effect is given to enemy.

--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class enemy : MonoBehaviour
 {
     gameManager gm;
+    public GameObject bomb;
+
+    float rangeX;
+    float rangeY;
+
     private void Awake()
     {
        gm = GameObject.Find("Spawner").GetComponent<gameManager>();
@@ -22,8 +27,12 @@ public class enemy : MonoBehaviour
 
     private void OnMouseDown()
     {
-
+        /*
+        rangeX = Random.Range(-6.36f, 5.62f);
+        rangeY = Random.Range(-1.66f, 3.4f); */
         Destroy(this.gameObject);
         gm.score++;
+        GameObject clone = Instantiate(bomb, new Vector3(gm.xpass, gm.ypass, 0f),Quaternion.identity);
+        Destroy(clone, 0.5f);
     }
 }
--- Assets/scripts/gameManager.cs
+++ Assets/scripts/gameManager.cs
@@ -7,10 +7,14 @@ using TMPro;
 public class gameManager : MonoBehaviour
 {
     public GameObject[] enemy;
+    public GameObject fire;
     
 
     float rangeX;
     float rangeY;
+
+    public float xpass;
+    public float ypass;
 
     public TMP_Text scoreText;
     public float score;
@@ -31,8 +35,18 @@ public class gameManager : MonoBehaviour
     {
         rangeX = Random.Range(-6.36f, 5.62f);
         rangeY = Random.Range(-1.66f, 3.4f);
+        xpass = rangeX;
+        ypass = rangeY;
         int prefabIndex = Random.Range(0, enemy.Length);
         Instantiate(enemy[prefabIndex], new Vector3(rangeX, rangeY, 0f), Quaternion.identity);
+       // StartCoroutine(monsterfire());
 
     }
+    /*
+    IEnumerator monsterfire()
+    {
+        yield return new WaitForSeconds(1f);
+        Instantiate(fire,new Vector3(rangeX,rangeY,0f),Quaternion.AngleAxis(-90,Vector3.forward));
+
+    }*/
 }
