b6b356d0a86b6b5507b8a416e64f8ab28fafbcf1
Shwetank Singh <ssingh.21.2019@gmail.com>
2023-07-22 19:38:25 +0530

    upload13
    Background and UI are made

--- Assets/scripts/enemyBullet.cs
+++ Assets/scripts/enemyBullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class enemyBullet : MonoBehaviour
 {
+    
     public GameObject player;
     public float speed = 10f;
     gameManager pp;
@@ -11,24 +12,36 @@ public class enemyBullet : MonoBehaviour
     void Start()
     {
         pp = GameObject.Find("Spawner").GetComponent<gameManager>();
+        
     }
 
     
     void Update()
     {
-        Vector3 direction = player.transform.position - this.transform.position;
-        float dist = direction.magnitude;
-        direction = direction.normalized;
         
-        float move = speed * Time.deltaTime;
-       
-        transform.Translate(move * direction);
-        if (transform.position.y<= -4.62f)
+        if (pp.isAlive != false)
         {
-            pp.health--;
-            //player.gameObject.GetComponent<Players>().phealth--;  // this was refering to the player prefab the changes made in prefab remain after the execution end.
-            Debug.Log(pp.health);
+            Vector3 direction = player.transform.position - this.transform.position;
+            float dist = direction.magnitude;
+            direction = direction.normalized;
+
+            float move = speed * Time.deltaTime;
+
+            transform.Translate(move * direction);
+
+            if (transform.position.y <= -4.62f)
+            {
+                pp.health--;
+                //player.gameObject.GetComponent<Players>().phealth--;  // this was refering to the player prefab the changes made in prefab remain after the execution end.
+                Debug.Log(pp.health);
+                Destroy(gameObject);
+            }
+
+        }
+        else
+        {
             Destroy(gameObject);
         }
+       
     }
 }
